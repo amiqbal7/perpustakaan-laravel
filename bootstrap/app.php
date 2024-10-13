4a7286d0ea0e2ad8dec923221e49a62752d30ebe
amiqbal7 <iqbalmuhammad7578@gmail.com>
2024-10-13 12:14:50 +0700

    fixerz

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*');
+        //
     })
-    // ->withMiddleware(function (Middleware $middleware) {
-    //     //
-    // })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
