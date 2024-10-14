b6cee847d7cbedb242f501f5fdd35f9659526559
amiqbal7 <iqbalmuhammad7578@gmail.com>
2024-10-14 16:13:55 +0700

    fix loanbook

--- resources/views/frontend/bookLoan/index.blade.php
+++ resources/views/frontend/bookLoan/index.blade.php
@@ -1,31 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+@section('title', 'Peminjaman Buku')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Peminjaman Buku</title>
+@section('head')
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
-</head>
+@endsection
 
-<body>
+@section('content')
     <div class="container-fluid">
 
         <!-- Page Heading -->
         <h1 class="h3 mb-2 text-gray-800">Peminjaman Buku.</h1>
         <p class="mb-4">DataTables is a third-party plugin that is used to generate the demo table below.
-            For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official
+            For more information about DataTables, please visit the <a target="_blank"
+                href="https://datatables.net">official
                 DataTables documentation</a>.</p>
 
         <!-- Card for User and Book Selection -->
@@ -68,7 +63,6 @@
                 </form>
             </div>
         </div>
-
         <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
         <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
@@ -89,6 +83,4 @@
         <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
 
     </div>
-</body>
-
-</html>
+@endsection
--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -10,7 +10,17 @@
     <meta name="author" content="">
 
     <title>@yield('title', 'Default Title')</title>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+     <!-- Custom fonts for this template-->
+     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+     <link
+         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+         rel="stylesheet">
+
+     <!-- Custom styles for this template-->
+     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+     <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
     @yield('head')
     <style>
         .active {
