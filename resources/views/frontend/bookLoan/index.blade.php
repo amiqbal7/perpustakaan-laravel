5469138d4142da6d937747750d9a5fc58bee0faf
amiqbal7 <iqbalmuhammad7578@gmail.com>
2024-10-14 16:02:17 +0700

    add head book-loan

--- resources/views/frontend/bookLoan/index.blade.php
+++ resources/views/frontend/bookLoan/index.blade.php
@@ -1,6 +1,14 @@
-@section('title', 'Peminjaman Buku')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('head')
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Peminjaman Buku</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
@@ -9,9 +17,9 @@
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
-@endsection
+</head>
 
-@section('content')
+<body>
     <div class="container-fluid">
 
         <!-- Page Heading -->
@@ -60,5 +68,27 @@
                 </form>
             </div>
         </div>
+
+        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+        <!-- Page level plugins -->
+        <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+        <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+        <!-- Page level custom scripts -->
+        <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+        <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
+        <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
+
     </div>
-@endsection
+</body>
+
+</html>
